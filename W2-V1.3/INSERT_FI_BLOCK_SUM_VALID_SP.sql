8844f9662147e69372592812f4049a3f4bbb01aa
Mohamed Gawad <gowadsss@gmail.com>
2019-03-04 15:15:05 +0300

    block storedProcedure

---
+++ W2-V1.3/INSERT_FI_BLOCK_SUM_VALID_SP.sql
@@ -0,0 +1,147 @@
+create or replace PROCEDURE INSERT_FI_BLOCK_SUM_VALID_SP (INPUT_VAL IN XMLTYPE , FI_BLOCK_SUM_VALID_ID_OUT OUT VARCHAR2) AS 
+BEGIN
+
+
+  /*
+
+<InsertFISummaryValidation xmlns:ns0="http://BEA_Solution_Block_Module/ProcessLib/DispatchFIInput" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="ns0:InsertFISummaryValidation">
+  <fi_Block_Summary_Validation>
+    <msg_uid>1800100001040-S1211289600755230397</msg_uid>
+    <msg_mode xsi:nil="true"/>
+    <fi_code>90010</fi_code>
+    <trans_date>2018-11-22 11:15:12.550</trans_date>
+    <is_acc_id>0</is_acc_id>
+    <involved_party_id>2834404758</involved_party_id>
+    <involved_party_id_type>RID</involved_party_id_type>
+    <nat_cd>MLI</nat_cd>
+    <amt_val>10000.0</amt_val>
+    <amt_cur>SAR</amt_cur>
+  </fi_Block_Summary_Validation>
+  <fi_Block_Summary_Validation>
+    <msg_uid>1800100001040-S1211289618921989681</msg_uid>
+    <msg_mode xsi:nil="true"/>
+    <fi_code>90055</fi_code>
+    <trans_date>2018-11-22 11:15:12.568</trans_date>
+    <is_acc_id>0</is_acc_id>
+    <involved_party_id>2834404758</involved_party_id>
+    <involved_party_id_type>RID</involved_party_id_type>
+    <nat_cd>MLI</nat_cd>
+    <amt_val>10000.0</amt_val>
+    <amt_cur>SAR</amt_cur>
+  </fi_Block_Summary_Validation>
+  <fi_Block_Summary_Validation>
+    <msg_uid>1800100001040-S121128962570459940</msg_uid>
+    <msg_mode xsi:nil="true"/>
+    <fi_code>90030</fi_code>
+    <trans_date>2018-11-22 11:15:12.574</trans_date>
+    <is_acc_id>0</is_acc_id>
+    <involved_party_id>2834404758</involved_party_id>
+    <involved_party_id_type>RID</involved_party_id_type>
+    <nat_cd>MLI</nat_cd>
+    <amt_val>10000.0</amt_val>
+    <amt_cur>SAR</amt_cur>
+  </fi_Block_Summary_Validation>
+</InsertFISummaryValidation>
+
+
+  */
+
+
+
+  FOR r IN
+      (SELECT ExtractValue(Value(p),'/fi_Block_Summary_Validation/msg_uid/text()') AS  MSG_UID,
+      Extract(Value(p),'/fi_Block_Summary_Validation/msg_mode/text()') As MSG_MODE,
+      Extract(Value(p),'/fi_Block_Summary_Validation/fi_code/text()') As FI_CODE,
+      Extract(Value(p),'/fi_Block_Summary_Validation/trans_date/text()') As TRANS_DATE,
+--      Extract(Value(p),'/fi_Block_Summary_Validation/is_involved_party/text()') As IS_INVOLVED_PARTY,
+      Extract(Value(p),'/fi_Block_Summary_Validation/is_acc_id/text()') As IS_ACC_ID,
+      Extract(Value(p),'/fi_Block_Summary_Validation/involved_party_id/text()') As INVOLVED_PARTY_ID,
+      Extract(Value(p),'/fi_Block_Summary_Validation/involved_party_id_type/text()') As INVOLVED_PARTY_ID_TYPE,
+      Extract(Value(p),'/fi_Block_Summary_Validation/nat_cd/text()') As NAT_CODE,
+      Extract(Value(p),'/fi_Block_Summary_Validation/trgt_amt_val/text()').getnumberval() As TRGT_AMT_VAL,
+      Extract(Value(p),'/fi_Block_Summary_Validation/trgt_amt_cur/text()') As TRGT_AMT_CUR,
+      Extract(Value(p),'/fi_Block_Summary_Validation/acc_num/text()') As ACC_NUM,
+      Extract(Value(p),'/fi_Block_Summary_Validation/is_iban/text()') As IS_IBAN,
+      Extract(Value(p),'/fi_Block_Summary_Validation/trgt_acc_prd/text()') As TRGT_ACC_PRD,
+      Extract(Value(p),'/fi_Block_Summary_Validation/trgt_depost_prd/text()') As TRGT_DEPOST_PRD,
+      Extract(Value(p),'/fi_Block_Summary_Validation/trgt_safs_prd/text()') As TRGT_SAFS_PRD,
+      Extract(Value(p),'/fi_Block_Summary_Validation/exe_acc_flag/text()') As EXE_ACC_FLAG,
+      Extract(Value(p),'/fi_Block_Summary_Validation/exe_depost_flag/text()') As EXE_DEPOST_FLAG,
+      Extract(Value(p),'/fi_Block_Summary_Validation/exe_safs_flag/text()') As EXE_SAFS_FLAG,
+      Extract(Value(p),'/fi_Block_Summary_Validation/info_jnt_acc_flag/text()') As INFO_JNT_ACC_FLAG,
+      Extract(Value(p),'/fi_Block_Summary_Validation/info_jnt_depost_flag/text()') As INFO_JNT_DEPOST_FLAG,
+      Extract(Value(p),'/fi_Block_Summary_Validation/info_shrs_flag/text()') As INFO_SHRS_FLAG,
+      Extract(Value(p),'/fi_Block_Summary_Validation/blocked_amt/text()').getnumberval() As BLOCKED_AMT,
+      Extract(Value(p),'/fi_Block_Summary_Validation/pending_amt/text()').getnumberval() As PENDING_AMT
+      FROM TABLE(XMLSequence(Extract(INPUT_VAL,'/InsertFISummaryValidation/fi_Block_Summary_Validation'))) p)
+
+      LOOP
+   begin
+    INSERT INTO FI_BLOCK_SUMMARY_VALIDATION 
+    (
+      MSG_UID,
+      MSG_MODE,
+      FI_CODE,
+      TRANS_DATE,
+--      IS_INVOLVED_PARTY,
+      IS_ACC_ID,
+      INVOLVED_PARTY_ID,
+      INVOLVED_PARTY_ID_TYPE,
+      NAT_CD,
+      TRGT_AMT_VAL,
+      TRGT_AMT_CUR,
+      ACC_NUM,
+      IS_IBAN,
+      TRGT_ACC_PRD,
+      TRGT_DEPOST_PRD,
+      TRGT_SAFS_PRD,
+      EXE_ACC_FLAG,
+      EXE_DEPOST_FLAG,
+      EXE_SAFS_FLAG,
+      INFO_JNT_ACC_FLAG,
+      INFO_JNT_DEPOST_FLAG,
+      INFO_SHRS_FLAG,
+      BLOCKED_AMT,
+      PENDING_AMT
+    )
+    VALUES
+    (
+      r.MSG_UID,
+      r.MSG_MODE,
+      r.FI_CODE,
+      to_date(r.TRANS_DATE,'YYYY-MM-DD HH24:MI:SS'),
+--      r.IS_INVOLVED_PARTY,
+      r.IS_ACC_ID,
+      r.INVOLVED_PARTY_ID,
+      r.INVOLVED_PARTY_ID_TYPE,
+      r.NAT_CODE,
+      r.TRGT_AMT_VAL,
+      r.TRGT_AMT_CUR,
+      r.ACC_NUM,
+      r.IS_IBAN,
+      r.TRGT_ACC_PRD,
+      r.TRGT_DEPOST_PRD,
+      r.TRGT_SAFS_PRD,
+      r.EXE_ACC_FLAG,
+      r.EXE_DEPOST_FLAG,
+      r.EXE_SAFS_FLAG,
+      r.INFO_JNT_ACC_FLAG,
+      r.INFO_JNT_DEPOST_FLAG,
+      r.INFO_SHRS_FLAG,
+      r.BLOCKED_AMT,
+      r.PENDING_AMT
+    )RETURNING FI_BLOCK_SUMMARY_VALIDATION_ID INTO FI_BLOCK_SUM_VALID_ID_OUT;
+
+  end;
+  END LOOP;
+
+
+  EXCEPTION
+  WHEN OTHERs THEN
+      Raise_application_error(-20322, 'UNKNOWN ERROR>>'|| SQLERRM); 
+
+
+
+
+
+END INSERT_FI_BLOCK_SUM_VALID_SP;
